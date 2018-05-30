76ae469dd12881c65a43ac592632364eb4f9e3bc
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-05-29 21:46:54 -0400

    modified js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-// Enemies our player must avoid
-
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
 
 
 let ScoreOutput = document.querySelector('.cross-output');
@@ -194,31 +187,8 @@ let player = new Player(200, 400);
 displayEnemy();
 displayRocks();
 
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-
-// Draw the enemy on the screen, required method for game
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method. 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
